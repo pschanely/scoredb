5f1aa544e0ea0511ef19abbfc3abeb0d400af0b0
Pierce Lopez <pierce.lopez@gmail.com>
2015-12-17 13:43:24 -0500

    brute-force impl reading csv file header

--- brute/brute.go
+++ brute/brute.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"log"
+	"os"
+	"strings"
 )
 
 var (
@@ -13,5 +17,32 @@ var (
 
 func main() {
 	flag.Parse()
+
+	if len(*csvfile) == 0 || len(*query) == 0 {
+		fmt.Printf("ERROR: --csvfile and --query must be specified\n")
+		flag.PrintDefaults()
+		os.Exit(2)
+	}
 	fmt.Printf("csvfile=%s query=%s limit=%d\n", *csvfile, *query, *limit)
+
+	csvReader, err := os.Open(*csvfile)
+	if err != nil {
+		log.Fatalf("ERROR: failed to open csvfile: %s\n", err)
+	}
+
+	csvScanner := bufio.NewScanner(csvReader)
+	csvScanner.Split(bufio.ScanLines)
+
+	if !csvScanner.Scan() {
+		log.Fatalf("ERROR: no header line in csvfile\n")
+	}
+	header := csvScanner.Text()
+	fields := strings.Split(header, ",")
+
+	invFields := make(map[string]int)
+	for i, f := range fields {
+		invFields[f] = i
+	}
+
+	fmt.Printf("fields=%v invFields=%v\n", fields, invFields)
 }
