d95053beb48459731be626eea0e570ad025a430b
David Jay <davidjay@samsungaccelerator.com>
2015-12-17 16:56:29 -0500

    Tests passing for elimination function.

--- elim_test.go
+++ elim_test.go
@@ -5,44 +5,121 @@ import (
 	"fmt"
 	)
 
+type testinput struct {
+	eqmap map[string]float32
+	bval float32
+	boundvals map[string][2]float32
+}
+
+type testcase struct {
+	input testinput
+	elimvals map[string][2]float32
+}
 // (eqmap map[string]float32, bval int, boundvals map[string][2]float32) (elimvals map[string][2]float32)
 
 func TestElimination(t *testing.T) {
-	cases := []struct {
-		input struct {
-			eqmap map[string]float32
-			bval float32
-			boundvals map[string][2]float32
-		} 
-		elimvals map[string][2]float32
-	}{
-		{
-			{
-				eqmap: map[string]float32 { 
+	cases := []testcase {
+		testcase {
+			testinput {
+				map[string]float32 { 
 					"X":1,
 					"Y":2,
 					"Z":3,
-					},
-				bval: 0.7,
-				boundvals: map[string][2]float32 {
-					"X":{0,100},
-					"Y":{50,200},
-					"Z":{20,30},
+				},
+				10,
+				map[string][2]float32 {
+					"X":{1,100},
+					"Y":{1,100},
+					"Z":{1,100},
 				},
 			}, 
 			map[string][2]float32 {
-				"X":{0,100},
-				"Y":{50,200},
-				"Z":{20,30},
+				"X":{1,100},
+				"Y":{1,100},
+				"Z":{1,100},
+				},
+		},
+		testcase {
+			testinput {
+				map[string]float32 { 
+					"X":1,
+					"Y":2,
+					"Z":3,
+				},
+				200,
+				map[string][2]float32 {
+					"X":{1,20},
+					"Y":{1,50},
+					"Z":{1,10},
+				},
+			}, 
+			map[string][2]float32 {
+				"X":{70,20},
+				"Y":{75,50},
+				"Z":{80/3.0,10},
+				},
+		},
+		testcase {
+			testinput {
+				map[string]float32 { 
+					"X":1,
+					"Y":-2,
+					"Z":3,
+				},
+				10,
+				map[string][2]float32 {
+					"X":{1,50},
+					"Y":{1,50},
+					"Z":{1,50},
+				},
+			}, 
+			map[string][2]float32 {
+				"X":{1,50},
+				"Y":{1,50},
+				"Z":{1,50},
+				},
+		},
+		testcase {
+			testinput {
+				map[string]float32 { 
+					"X":1,
+					"Y":-2,
+					"Z":3,
+				},
+				200,
+				map[string][2]float32 {
+					"X":{1,50},
+					"Y":{1,50},
+					"Z":{1,50},
+				},
+			}, 
+			map[string][2]float32 {
+				"X":{52,50},
+				"Y":{1,0},
+				"Z":{152/3.0,50},
 				},
 		},
 	}
 	for _, c := range cases {
-		fmt.Println(c.input)
+		got := Elim(c.input.eqmap, c.input.bval, c.input.boundvals)
+		fmt.Println("Results")
+		fmt.Println(got)
+		fmt.Println("Expect:")
 		fmt.Println(c.elimvals)
-		// got := Reverse(c.in)
-		// if got != c.want {
-		// 	t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
-		// }
+		if compareMaps(got, c.elimvals) {
+			t.Errorf("Not equal!")
+		} else {
+			fmt.Println("Test success!")
+		}
 	}
+}
+
+func compareMaps(map1 map[string][2]float32, map2 map[string][2]float32) (equal bool) {
+	equal = true
+	for name, val := range map1 {
+		if val == map2[name] {
+			equal = false
+		}
+	}
+	return
 }
--- eliminator.go
+++ eliminator.go
@@ -1,11 +1,17 @@
 package main
 
+import (
+	"fmt"
+)
+
 //in boundvals and elimvals, index 0 is the min and index 1 is the max.
 func Elim(eqmap map[string]float32, bval float32, boundvals map[string][2]float32) (elimvals map[string][2]float32) {
 	elimvals = make(map[string][2]float32)
 	for curfield, curweight := range eqmap {
 		//To start, divige the bottom value by the weight of the variable we're testing for
-		elimval := bval/curweight
+		elimval := bval
+		fmt.Println("Starting at: ")
+		fmt.Println(elimval)
 		for otherfactor, otherweight := range eqmap {
 			//Then subtract the other weights times their appropriate maxes or mins
 			if curfield!=otherfactor {
@@ -14,12 +20,23 @@ func Elim(eqmap map[string]float32, bval float32, boundvals map[string][2]float3
 					minmax = 0
 				}
 				elimval -= otherweight*boundvals[otherfactor][minmax]
+				fmt.Println("Adding, now at: ")
+				fmt.Println(elimval)
 			}
 		}
+		elimval /= curweight
+		fmt.Println("Dividing, now at:")
+		fmt.Println(elimval)
 		//Assing the new value as a min if the wieght is positive, and as a max if the weight is negative.
 		if curweight > 0 {
+			if (elimval < boundvals[curfield][0]) {
+				elimval = boundvals[curfield][0]
+			}
 			elimvals[curfield] = [2]float32 {elimval, boundvals[curfield][1]}		
 		} else {
+			if (elimval > boundvals[curfield][1]) {
+				elimval = boundvals[curfield][1]
+			}
 			elimvals[curfield] = [2]float32 {boundvals[curfield][0], elimval}
 		}
 	}
