a97970db77cfba8f4218043faae29b4425614641
Phillip Schanely <phil@schanely.com>
2015-12-17 10:16:01 -0500

    docitr interface

---
+++ docitr.go
@@ -0,0 +1,14 @@
+package scoredb
+
+import (
+)
+
+type DocItr interface {
+	// An iterator over (document id, score) values. It is initially not positioned; a call to Next() is required before this has a real value
+	Name() string
+	SetBounds(min, max float32) bool // return false if the iterator is now known to not produce any more values
+	Next() bool // return true if there is another value
+	DocId() int64 // doc id of current result; 
+	Score() float32 // score of current result; 
+}
+
