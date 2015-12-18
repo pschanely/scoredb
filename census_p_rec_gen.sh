ec0efaaa4f462c6e1b91275b4cf6d678fb61220b
Pierce Lopez <pierce.lopez@gmail.com>
2015-12-18 15:33:09 -0500

    census 1990 csv generator - remove "weight" column, add "wages" column

--- census_p_rec_gen.sh
+++ census_p_rec_gen.sh
@@ -16,9 +16,9 @@ weekly_work_hours
 last_week_work_hours
 carpool_riders
 income
+wages
 poverty_percentage
 sex
-weight
 military_service_years
 "
 
@@ -37,9 +37,9 @@ weekly_work_hours      = substr($0,125,2);
 last_week_work_hours   = substr($0,93,2);
 carpool_riders         = substr($0,104,1);
 income                 = substr($0,133,6);
+wages                  = substr($0,139,6);
 poverty_percentage     = substr($0,41,3);
 sex                    = substr($0,11,1);
-weight                 = substr($0,18,4);
 military_service_years = substr($0,83,2);
 
 children = (children == "00") ? 0 : int(children) - 1;
@@ -54,8 +54,8 @@ printf("r%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
        last_week_work_hours,
        carpool_riders,
        income,
+       wages,
        poverty_percentage,
        sex,
-       weight,
        military_service_years)
 } ' <$INPUT >>$OUTPUT
