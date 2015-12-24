4d4f3adcd501dbd729966ffe4031737ab7e43851
Pierce Lopez <pierce.lopez@gmail.com>
2015-12-23 21:05:14 -0500

    make census1990 processing script less redundant
    also make it easier to specify awk binary to use

--- dataset_tools/census_p_rec_gen.sh
+++ dataset_tools/census_p_rec_gen.sh
@@ -7,6 +7,9 @@
 INPUT=census1990_people.dat
 OUTPUT=census1990_people.csv
 
+# for this dataset, gawk output is different than mawk or nawk, for 7 records (out of millions)
+AWK=${AWK:-awk}
+
 COLUMNS="
 age
 children
@@ -28,34 +31,21 @@ military_service_years
  done
  printf '\n'                ) >$OUTPUT
 
-awk '{
-age                    = substr($0,15,2);
-children               = substr($0,89,2);
-depart_for_work        = substr($0,105,4);
-traveltime_to_work     = substr($0,109,2);
-weekly_work_hours      = substr($0,125,2);
-last_week_work_hours   = substr($0,93,2);
-carpool_riders         = substr($0,104,1);
-income                 = substr($0,133,6);
-wages                  = substr($0,139,6);
-poverty_percentage     = substr($0,41,3);
-sex                    = substr($0,11,1);
-military_service_years = substr($0,83,2);
-
+$AWK '{
+children = substr($0,89,2);
 children = (children == "00") ? 0 : int(children) - 1;
 
-printf("r%d,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n",
-       NR,
-       age,
-       children,
-       depart_for_work,
-       traveltime_to_work,
-       weekly_work_hours,
-       last_week_work_hours,
-       carpool_riders,
-       income,
-       wages,
-       poverty_percentage,
-       sex,
-       military_service_years)
+printf("r%d,", NR);
+printf("%s,",  substr($0, 15,2));  # age
+printf("%s,",  children        );  # children
+printf("%s,",  substr($0,105,4));  # depart_for_work
+printf("%s,",  substr($0,109,2));  # traveltime_to_work
+printf("%s,",  substr($0,125,2));  # weekly_work_hours
+printf("%s,",  substr($0, 93,2));  # last_week_work_hours
+printf("%s,",  substr($0,104,1));  # carpool_riders
+printf("%s,",  substr($0,133,6));  # income
+printf("%s,",  substr($0,139,6));  # wages
+printf("%s,",  substr($0, 41,3));  # poverty_percentage
+printf("%s,",  substr($0, 11,1));  # sex
+printf("%s\n", substr($0, 83,2));  # military_service_years
 } ' <$INPUT >>$OUTPUT
